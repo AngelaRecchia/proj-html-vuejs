4c0c9300dc66eae5d5b25eebd257af889051dc72
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-20 14:44:05 +0200

    projects transitions

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,16 +10,56 @@
         ></span>
       </div>
 
-      <div id="types" v-for="(elem, index) in types" :key="index">
-        {{ elem }}
-      </div>
-
-      <div class="row row-cols-3 gy-4">
-        <div v-for="(elem, index) in projects" :key="index" class="box col">
-          <img :src="require('../assets/img/' + elem.img)" :alt="elem.name" />
-          <h5 class="name text-white">{{ elem.name }}</h5>
+      <div id="types" class="my-4">
+        <div
+          id="type"
+          class="text"
+          v-for="(elem, index) in types"
+          :key="index"
+          @click.prevent="select(index)"
+          :class="{ selected: isSelected == index }"
+        >
+          {{ elem }}
         </div>
       </div>
+
+      <transition-group name="inOut" class="row row-cols-3 gy-4 inOutGroup">
+        <div
+          v-for="(elem, index) in projects"
+          :key="index"
+          class="box col inOutItem"
+          v-show="filter(elem.type)"
+        >
+          <img
+            class="projImg"
+            :src="require('../assets/img/' + elem.img)"
+            :alt="elem.name"
+          />
+          <h5 class="name text-white">{{ elem.name }}</h5>
+
+          <p class="infos fromTop">
+            <span>
+              <img
+                src="https://api.iconify.design/simple-line-icons:user.svg"
+                alt="icon user"
+              />
+              Codings
+            </span>
+
+            <span>
+              <img
+                src="https://api.iconify.design/simple-line-icons:clock.svg"
+                alt="icon clock"
+              />
+              4 Months
+            </span>
+          </p>
+
+          <p class="infos fromBottom">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+      </transition-group>
     </div>
   </section>
 </template>
@@ -31,6 +71,7 @@ export default {
   data() {
     return {
       projects: Projects,
+      isSelected: 0,
     };
   },
   computed: {
@@ -41,8 +82,19 @@ export default {
           if (!types.includes(type)) types.push(type);
         }
       }
-
       return types;
+    },
+  },
+  methods: {
+    select(type) {
+      this.isSelected = type;
+    },
+
+    filter(elem) {
+      if (this.isSelected == 0) return true;
+
+      if (elem.includes(this.types[this.isSelected])) return true;
+      else return false;
     },
   },
 };
@@ -52,23 +104,125 @@ export default {
 @import "@/style/commons.scss";
 #projects {
   background-color: $blueblack;
+  height: 1900px;
+
+  .inOutGroup {
+    overflow: hidden;
+    backface-visibility: hidden;
+    z-index: 1;
+  }
+
+  .inOut {
+    position: relative;
+    backface-visibility: hidden;
+    z-index: 1;
+  }
+
+  .inOut-move {
+    transition: all 600ms ease-in-out 50ms;
+  }
+
+  .inOut-enter-active {
+    transition: all 300ms ease-out;
+  }
+
+  .inOut-leave-active {
+    transition: all 200ms ease-in;
+    position: absolute;
+    z-index: 0;
+  }
+
+  .inOut-enter,
+  .inOut-leave-to {
+    opacity: 0;
+  }
+
+  .inOut-enter {
+    transform: scale(0.9);
+  }
 
   .box {
     border-radius: 10px;
     position: relative;
     display: flex;
     justify-content: center;
+    overflow: hidden;
+    & > * {
+      transition: all 0.6s ease-in-out;
+    }
 
-    img {
+    .projImg {
       width: 100%;
       border-radius: 10px;
       opacity: 0.5;
     }
-
     .name {
       position: absolute;
       bottom: 7%;
       width: 78%;
+    }
+
+    .infos {
+      position: absolute;
+      width: 78%;
+      color: $lightgrey;
+      font-size: 15px;
+
+      span {
+        margin: 0 5px;
+      }
+
+      img {
+        width: 20px;
+        filter: invert(92%) sepia(7%) saturate(77%) hue-rotate(148deg)
+          brightness(82%) contrast(89%);
+      }
+    }
+
+    .fromBottom {
+      bottom: -30%;
+    }
+
+    .fromTop {
+      text-align: center;
+      top: -30%;
+    }
+
+    &:hover {
+      .projImg {
+        filter: blur(2px);
+        opacity: 0.4;
+      }
+
+      .name {
+        bottom: 20%;
+      }
+
+      .fromBottom {
+        bottom: 5%;
+      }
+
+      .fromTop {
+        top: 5%;
+      }
+    }
+  }
+
+  #types {
+    display: flex;
+    justify-content: space-evenly;
+
+    #type {
+      text-transform: uppercase;
+      padding: 10px 15px;
+      border-radius: 5px;
+      color: $lightgrey;
+      font-weight: 500;
+      cursor: pointer;
+    }
+
+    .selected {
+      background-color: rgba($color: $green, $alpha: 0.2);
     }
   }
 }
